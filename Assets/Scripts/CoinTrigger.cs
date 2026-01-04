16506ef8172a980fa7f16198b70a1ff671ebdf0d
Marcim2489 <marciohgimenes@hotmail.com.br>
2026-01-03 22:40:46 -0300

    ajustes

--- Assets/Scripts/CoinTrigger.cs
+++ Assets/Scripts/CoinTrigger.cs
@@ -5,9 +5,14 @@ public class CoinTrigger : MonoBehaviour
     [SerializeField]private float speed = 5;
     [SerializeField]private Coin coin;
 
-
+    bool triggered;
     public void Trigger()
     {
+        if (triggered)
+        {
+            return;
+        }
+        triggered = true;
         Coin c = Instantiate(coin, transform.position, transform.rotation);
         c.gameObject.GetComponent<Rigidbody2D>().linearVelocityX = -speed;
         Destroy(gameObject);
--- Assets/Scripts/RubyTrigger.cs
+++ Assets/Scripts/RubyTrigger.cs
@@ -5,9 +5,13 @@ public class RubyTrigger : MonoBehaviour
     [SerializeField]private float speed = 5;
     [SerializeField]private Ruby ruby;
 
-
+    bool triggered;
     public void Trigger()
     {
+        if (triggered)
+        {
+            return;
+        }
         Ruby r = Instantiate(ruby, transform.position, transform.rotation);
         r.gameObject.GetComponent<Rigidbody2D>().linearVelocityX = -speed;
         Destroy(gameObject);
